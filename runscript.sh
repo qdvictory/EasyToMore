baee257d2d856cf0ba536b8b3afbe69013cff95a
qdvictory <qdvictory@gmail.com>
2014-06-25 12:34:50 +0800

    支持新版fir.im

--- runscript.sh
+++ runscript.sh
@@ -8,7 +8,9 @@ if [ "${CONFIGURATION}" = "Debug" ]; then
 
 ############################开发者配置#################################
 #icon地址(相对于项目根目录)
-pathtoartwork="iFurniture/icon/120.png"
+pathtoartwork="EasytoMore/iTunesArtwork.png"
+#Note:token 用于识别用户身份, 如果请求不带token 作为匿名访问处理. token可以在用户信息中找到(只有上传过应用的用户才具有使用开发者API的权限)
+token="qingzixingxiugai"
 #####################################################################
 
 #获取app名
@@ -38,27 +40,27 @@ cd $CONFIGURATION_BUILD_DIR
 gitcommit=`git --git-dir="${gitpath}/.git" log -1 --oneline --pretty=format:'%s'`
 
 #fir.im上传第一步
-d=`curl "http://firapp.duapp.com/api/upload_url?appid="${appid}`
-#fir.im上传第二步
-postFile=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['postFile'];"`
-postIcon=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['postIcon'];"`
-shorturl=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['short'];"`
-curl -T ${target}.ipa ${postFile} -X PUT
-curl -T $CONFIGURATION_BUILD_DIR/iTunesArtwork ${postIcon} -X PUT
-#fir.im上传第三步
-postData='appid='${appid}'&short='${shorturl}'&version='${version}'&name='${displayname}
-if [ "${gitcommit}" ]; then
-gitcommit=$(perl -MURI::Escape -e 'print uri_escape("'"${gitcommit}"'");' "$2")
-postData=${postData}'&changelog='${gitcommit}
-fi
-r=`curl -X POST -d ${postData} -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" "http://firapp.duapp.com/api/finish"`
-short=`echo ${r}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['short'];"`
-#输出url
-`osascript -e 'tell app "System Events" to (display dialog "恭喜您，IPA上传完成。复制地址即可下载。\nhttp://fir.im/'${short}'" with title "IPA一键分享" buttons {"ok"})'`
+d=`curl "http://fir.im/api/v2/app/info/:"${appid}"?token="${token}`
+pkgkey=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['pkg']['key'];"`
+pkgurl=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['pkg']['url'];"`
+pkgtoken=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['pkg']['token'];"`
+iconkey=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['icon']['key'];"`
+iconurl=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['icon']['url'];"`
+icontoken=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['bundle']['icon']['token'];"`
+pkgid=`echo ${d}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['id'];"`
+
+up=`curl -F "key="${iconkey} -F "token="${icontoken} -F "file=@"$CONFIGURATION_BUILD_DIR"/iTunesArtwork" ${iconurl}`
+upipa=`curl -F "key="${pkgkey} -F "token="${pkgtoken} -F "file=@"${target}".ipa" ${pkgurl}`
+editinfo=`curl -d "changelog="${gitcommit}"&version="${version} -X PUT "http://fir.im/api/v2/app/"${pkgid}"?token="${token}`
+
 
 #删除临时文件
 rm -R $CONFIGURATION_BUILD_DIR/Payload
 rm ${target}.ipa
+
+short=`echo ${editinfo}| ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['short'];"`
+#输出url
+`osascript -e 'tell app "System Events" to (display dialog "恭喜您，IPA上传完成。复制地址即可下载。\nhttp://fir.im/'${short}'" with title "IPA一键分享" buttons {"ok"})'`
 
 fi
 exit 0
